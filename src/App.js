75519e7857c94fa8f971e9274d6f284bd5ca82f4
Lucas Almada <lucas.almadag7@gmail.com>
2022-08-31 12:30:19 -0300

    Change app text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<img src={logo} className="App-logo" alt="logo" />
-				<p>Have fun coding! Changed</p>
+				<p>Travis CI and AWS deploy working!</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
